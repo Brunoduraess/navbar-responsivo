233224628c553c706c7d1351d893e16eb3c22f69
Bruno Durães de Souza Gonçalves <113933881+Brunoduraess@users.noreply.github.com>
2022-09-20 15:57:55 -0300

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -75,14 +75,6 @@ $username = "Username";
             </div>
         </div>
     </header>
-    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <script>
-        $(function() {
-            $('[data-toggle="tooltip"]').tooltip()
-        })
-    </script>
-    <script src="script.js"></script>
 </body>
 
 </html>
